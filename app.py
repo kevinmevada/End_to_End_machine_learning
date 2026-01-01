f2e49cd1531073273e8d11ddf9851cbc0985a8cb
Kevin Mevada <mevadakevin@gmail.com>
2025-12-31 18:50:24 -0600

    Front End Completed

--- app.py
+++ app.py
@@ -1,44 +1,42 @@
-from flask import Flask,request,render_template
-import numpy as np
-import pandas as pd
+from flask import Flask, request, render_template
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-from sklearn.preprocessing import StandardScaler
+application = Flask(__name__)
+app = application
 
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
-
-application=Flask(__name__)
-
-app=application
-
-## Route for a home page
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata',methods=['GET','POST'])
+
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
-    else:
-        # POST part
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+    if request.method == 'POST':
+        form_data = request.form.to_dict()
 
+        data = CustomData(
+            gender=form_data.get('gender'),
+            race_ethnicity=form_data.get('ethnicity'),
+            parental_level_of_education=form_data.get('parental_level_of_education'),
+            lunch=form_data.get('lunch'),
+            test_preparation_course=form_data.get('test_preparation_course'),
+            reading_score=float(form_data.get('reading_score')),
+            writing_score=float(form_data.get('writing_score'))
         )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        
-        predict_pipline=PredictPipeline()
-        results=predict_pipline.predict(pred_df)
-        
-        return render_template('home.html',results=results[0])
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',debug=True)
+        pred_df = data.get_data_as_data_frame()
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+
+        return render_template(
+            'home.html',
+            results=results[0],
+            form_data=form_data
+        )
+
+    return render_template('home.html', form_data={})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
